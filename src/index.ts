7f7d809488eed4e04a5f3ebcd9ef66c27cf3438e
officialzoka <golldenhouse@gmail.com>
2023-01-14 18:20:49 +0200

    fix: build config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { range, uuid, normal } from "./helpers";
 import { Idigy } from "./types";
 
-export default function idigy(): Idigy {
-    return {
-        pattern: "xxxx-xxxx-xxxx-xxxx",
-        range,
-        uuid,
-        normal,
-    };
-}
+const idigy: Idigy = {
+    pattern: "xxxx-xxxx-xxxx-xxxx",
+    range,
+    uuid,
+    normal,
+};
+export default idigy;
